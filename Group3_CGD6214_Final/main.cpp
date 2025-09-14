8cd17ef1ee0b56607ab141ade3d77087e9fadb47
hmlaw <1211108826@student.mmu.edu.my>
2025-09-14 18:03:17 +0800

    delete glm

--- Group3_CGD6214_Final/main.cpp
+++ Group3_CGD6214_Final/main.cpp
@@ -1,9 +1,9 @@
 // main.cpp
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
 
 #include <vector>
 #include <string>
